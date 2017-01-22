fcde1d41b0f454687cb5ba14db8a654c57d59eb5
Rob Wilson <roobert@gmail.com>
2017-01-22 18:43:25 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './App.css';
+
+var json;
+var server = "http://localhost/kubernetes/"
+var api = "api/v1"
+var url = `${server}${api}/pods`
+
+
+function updateData() {
+  fetch(url,
+    {
+      mode: "no-cors",
+      method: "GET"
+  })
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    json = JSON.stringify(json)
+  });
+}
+
+class Pods extends React.Component {
+  constructor(props) {
+    super(props);
+    updateData();
+    this.state = {text: json};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      10000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    updateData();
+    this.setState({
+      text: json
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.text}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-fetch">
+        <Pods />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
